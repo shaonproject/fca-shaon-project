2af7f9e8f4fd81cd2f90fbbe5af4eacec30ded5c
Ivan Cotacte <cotactearmenion@gmail.com>
2023-10-04 17:31:41 +0800

    1

--- Extra/Src/Check_Update.js
+++ Extra/Src/Check_Update.js
@@ -4,8 +4,7 @@ module.exports = async function(Stable_Version) {
     const fs = require('fs');
     const Database = require('../Database');
     const { execSync } = require('child_process');
-    //make request https://raw.githubusercontent.com/KanzuXHorizon/Fca-Horizon-Remastered/main/package.json
-    const { body } = await got('https://raw.githubusercontent.com/KanzuXHorizon/Fca-Horizon-Remastered/main/package.json');
+    const { body } = await got('https://raw.githubusercontent.com/ivancotacte/fca-project-orion/main/package.json');
     const json = JSON.parse(body);
     const LocalVersion = require('../../package.json').version;
         if (Number(LocalVersion.replace(/\./g,"")) < Number(json.version.replace(/\./g,"")) && global.Fca.Require.FastConfig.Stable_Version.Accept == false || Stable_Version && Number(LocalVersion.replace(/\./g,"")) != Number(Stable_Version.replace(/\./g,""))) {
@@ -13,7 +12,7 @@ module.exports = async function(Stable_Version) {
             log.warn("[ FCA-UPDATE ] •","New Version, Ready to Update: " + LocalVersion + " -> " + Version);    
             await new Promise(resolve => setTimeout(resolve, 3000));
             try {
-                execSync(`npm install fca-horizon-remastered@${Version}`, { stdio: 'inherit' });
+                execSync(`npm install fca-project-orion@${Version}`, { stdio: 'inherit' });
                 log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                 await new Promise(resolve => setTimeout(resolve, 3000));
                 Database().set("Instant_Update", Date.now());
@@ -25,7 +24,7 @@ module.exports = async function(Stable_Version) {
                     console.log(err);  
                     log.warn("[ FCA-UPDATE ] •","Update Failed, Trying Another Method 1...");
                     await new Promise(resolve => setTimeout(resolve, 3000));
-                    execSync(`npm install fca-horizon-remastered@${Version} --force`, { stdio: 'inherit' });
+                    execSync(`npm install fca-project-orion@${Version} --force`, { stdio: 'inherit' });
                     log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                     await new Promise(resolve => setTimeout(resolve, 3000));
                     Database().set("Instant_Update", Date.now());
@@ -41,9 +40,9 @@ module.exports = async function(Stable_Version) {
                         log.info("[ FCA-UPDATE ] •","Cache Cleaned, Trying Another Method 2...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         //self delete fca-horizon-remastered folder from node_modules
-                        fs.rmdirSync((process.cwd() + "/node_modules/fca-horizon-remastered" || __dirname + '../../../fca-horizon-remastered'), { recursive: true });
+                        fs.rmdirSync((process.cwd() + "/node_modules/fca-project-orion" || __dirname + '../../../fca-project-orion'), { recursive: true });
                         await new Promise(resolve => setTimeout(resolve, 3000));
-                        execSync(`npm install fca-horizon-remastered@${Version}`, { stdio: 'inherit' });
+                        execSync(`npm install fca-project-orion@${Version}`, { stdio: 'inherit' });
                         log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         Database().set("Instant_Update", Date.now(), true);
