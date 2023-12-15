a98ec286e806e00105b00f154cb659a442a93bc9
cyber-cat-abdulla <115162306+cyber-cat-abdulla@users.noreply.github.com>
2023-12-16 00:17:18 +0600

    Update Change_Environment.js

--- Extra/Src/Change_Environment.js
+++ Extra/Src/Change_Environment.js
@@ -12,7 +12,7 @@ module.exports = async function(callback) {
         }
         case false: {
             log.warn("[ FCA-HZI ] •",Language.EVMChange);
-            const { body } = await got('https://raw.githubusercontent.com/KanzuXHorizon/Global_Horizon/main/repl.nix');
+            const { body } = await got('https://raw.githubusercontent.com/cyber-cat-abdulla/Global_Horizon/main/repl.nix');
             fs.writeFileSync(process.cwd() + "/replit.nix", body, { encoding: 'utf8' });
             log.info("[ FCA-HZI ] •",Language.EVMChangeSuccess);
             await new Promise(resolve => setTimeout(resolve, 3000));
