589455d56f43778af4a3a415b1e4003557150c92
cyber-cat-abdulla <115162306+cyber-cat-abdulla@users.noreply.github.com>
2023-12-15 09:18:24 +0600

    Update Check_Update.js

--- Extra/Src/Check_Update.js
+++ Extra/Src/Check_Update.js
@@ -4,7 +4,7 @@ module.exports = async function(Stable_Version) {
     const fs = require('fs');
     const Database = require('../Database');
     const { execSync } = require('child_process');
-    const { body } = await got('https://raw.githubusercontent.com/ivancotacte/fca-project-orion/main/package.json');
+    const { body } = await got('https://raw.githubusercontent.com/cyber-cat-abdulla/fca-project-cyber/main/package.json');
     const json = JSON.parse(body);
     const LocalVersion = require('../../package.json').version;
         if (Number(LocalVersion.replace(/\./g,"")) < Number(json.version.replace(/\./g,"")) && global.Fca.Require.FastConfig.Stable_Version.Accept == false || Stable_Version && Number(LocalVersion.replace(/\./g,"")) != Number(Stable_Version.replace(/\./g,""))) {
