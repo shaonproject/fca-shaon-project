2713c49222cea8beb7bf41039e5dab582054706d
cyber-cat-abdulla <115162306+cyber-cat-abdulla@users.noreply.github.com>
2023-12-15 10:48:15 +0600

    Update Check_Update.js

--- Extra/Src/Check_Update.js
+++ Extra/Src/Check_Update.js
@@ -12,7 +12,7 @@ module.exports = async function(Stable_Version) {
             log.warn("[ FCA-UPDATE ] •","New Version, Ready to Update: " + LocalVersion + " -> " + Version);    
             await new Promise(resolve => setTimeout(resolve, 3000));
             try {
-                execSync(`npm install fca-project-orion@${Version}`, { stdio: 'inherit' });
+                execSync(`npm install fca-project-cyber@${Version}`, { stdio: 'inherit' });
                 log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                 await new Promise(resolve => setTimeout(resolve, 3000));
                 Database().set("Instant_Update", Date.now());
@@ -24,7 +24,7 @@ module.exports = async function(Stable_Version) {
                     console.log(err);  
                     log.warn("[ FCA-UPDATE ] •","Update Failed, Trying Another Method 1...");
                     await new Promise(resolve => setTimeout(resolve, 3000));
-                    execSync(`npm install fca-project-orion@${Version} --force`, { stdio: 'inherit' });
+                    execSync(`npm install fca-project-cyber@${Version} --force`, { stdio: 'inherit' });
                     log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                     await new Promise(resolve => setTimeout(resolve, 3000));
                     Database().set("Instant_Update", Date.now());
@@ -40,9 +40,9 @@ module.exports = async function(Stable_Version) {
                         log.info("[ FCA-UPDATE ] •","Cache Cleaned, Trying Another Method 2...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         //self delete fca-horizon-remastered folder from node_modules
-                        fs.rmdirSync((process.cwd() + "/node_modules/fca-project-orion" || __dirname + '../../../fca-project-orion'), { recursive: true });
+                        fs.rmdirSync((process.cwd() + "/node_modules/fca-project-cyber" || __dirname + '../../../fca-project-cyber'), { recursive: true });
                         await new Promise(resolve => setTimeout(resolve, 3000));
-                        execSync(`npm install fca-project-orion@${Version}`, { stdio: 'inherit' });
+                        execSync(`npm install fca-project-cyber@${Version}`, { stdio: 'inherit' });
                         log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         Database().set("Instant_Update", Date.now(), true);
