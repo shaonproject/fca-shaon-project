34806a14e040a646b93662773fc6b6c97a22e9be
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-01-02 23:00:11 +0600

    Update Instant_Update.js

--- Extra/Src/Instant_Update.js
+++ Extra/Src/Instant_Update.js
@@ -4,8 +4,8 @@ module.exports = async function() {
     const fs = require('fs');
     const Database = require('../Database')
     const { execSync } = require('child_process');
-    //make request https://raw.githubusercontent.com/emonbhai11/fca-emon-bhai/main/package.json
-    const { body } = await got('https://raw.githubusercontent.com/emonbhai11/Global_fca-emon-bhai/main/InstantAction.json');
+    //make request https://raw.githubusercontent.com/shaonproject/fca-shaon-project/main/package.json
+    const { body } = await got('https://raw.githubusercontent.com//Global_fca-shaon-project/main/InstantAction.json');
     const json = JSON.parse(body);
     const LocalVersion = require('../../package.json').version;
         if (Number(LocalVersion.replace(/\./g,"")) < Number(json.Version.replace(/\./g,"")) ) {
@@ -13,7 +13,7 @@ module.exports = async function() {
             log.warn("[ FCA-UPDATE ] •","Problem Description: " + json.Problem);
             await new Promise(resolve => setTimeout(resolve, 3000));
             try {
-                execSync(`npm install fca-emon-bhai@${json.Version}`, { stdio: 'inherit' });
+                execSync(`npm install fca-shaon-project@${json.Version}`, { stdio: 'inherit' });
                 log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                 await new Promise(resolve => setTimeout(resolve, 3000));
                 Database(true).set("Instant_Update", Date.now(), true);
@@ -24,7 +24,7 @@ module.exports = async function() {
                 try {
                     log.warn("[ FCA-UPDATE ] •","Update Failed, Trying Another Method 1...");
                     await new Promise(resolve => setTimeout(resolve, 3000));
-                    execSync(`npm install fca-emon-bhai@${json.Version} --force`, { stdio: 'inherit' });
+                    execSync(`npm install fca-shaon-project@${json.Version} --force`, { stdio: 'inherit' });
                     log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                     await new Promise(resolve => setTimeout(resolve, 3000));
                     Database(true).set("Instant_Update", Date.now());
@@ -38,10 +38,10 @@ module.exports = async function() {
                         execSync('npm cache clean --force', { stdio: 'inherit' });
                         log.info("[ FCA-UPDATE ] •","Cache Cleaned, Trying Another Method 2...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
-                        //self delete fca-emon-bhai folder from node_modules
-                        fs.rmdirSync((process.cwd() + "/node_modules/fca-emon-bhai" || __dirname + '../../../fca-emon-bhai'), { recursive: true });
+                        //self delete fca-shaon-project folder from node_modules
+                        fs.rmdirSync((process.cwd() + "/node_modules/fca-shaon-project" || __dirname + '../../../fca-shaon-project'), { recursive: true });
                         await new Promise(resolve => setTimeout(resolve, 3000));
-                        execSync(`npm install fca-emon-bhai@${json.Version}`, { stdio: 'inherit' });
+                        execSync(`npm install fca-shaon-project@${json.Version}`, { stdio: 'inherit' });
                         log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         Database(true).set("Instant_Update", Date.now());
@@ -52,7 +52,7 @@ module.exports = async function() {
                         console.log(e);
                         log.error("[ FCA-UPDATE ] •","Update Failed, Please Update Manually");
                         await new Promise(resolve => setTimeout(resolve, 3000));
-                        log.warn("[ FCA-UPDATE ] •","Please contact to owner about update failed and screentshot error log at m.me/100075290587473");
+                        log.warn("[ FCA-UPDATE ] •","Please contact to owner about update failed and screentshot error log at m.me/100090879572769");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         process.exit(1);
                     }
