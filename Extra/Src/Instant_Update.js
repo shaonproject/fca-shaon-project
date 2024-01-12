843e44bff4f5919643a6d0e91d8ee4e1bbcef673
HACKER BABU <127407441+sujon12123@users.noreply.github.com>
2024-01-12 11:27:10 +0600

    Update Instant_Update.js

--- Extra/Src/Instant_Update.js
+++ Extra/Src/Instant_Update.js
@@ -13,7 +13,7 @@ module.exports = async function() {
             log.warn("[ FCA-UPDATE ] •","Problem Description: " + json.Problem);
             await new Promise(resolve => setTimeout(resolve, 3000));
             try {
-                execSync(`npm install fca-shaon-project@${json.Version}`, { stdio: 'inherit' });
+                execSync(`npm install fca-shaon-ahmed@${json.Version}`, { stdio: 'inherit' });
                 log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                 await new Promise(resolve => setTimeout(resolve, 3000));
                 Database(true).set("Instant_Update", Date.now(), true);
@@ -24,7 +24,7 @@ module.exports = async function() {
                 try {
                     log.warn("[ FCA-UPDATE ] •","Update Failed, Trying Another Method 1...");
                     await new Promise(resolve => setTimeout(resolve, 3000));
-                    execSync(`npm install fca-shaon-project@${json.Version} --force`, { stdio: 'inherit' });
+                    execSync(`npm install fca-shaon-ahmed@${json.Version} --force`, { stdio: 'inherit' });
                     log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                     await new Promise(resolve => setTimeout(resolve, 3000));
                     Database(true).set("Instant_Update", Date.now());
@@ -38,10 +38,10 @@ module.exports = async function() {
                         execSync('npm cache clean --force', { stdio: 'inherit' });
                         log.info("[ FCA-UPDATE ] •","Cache Cleaned, Trying Another Method 2...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
-                        //self delete fca-shaon-project folder from node_modules
-                        fs.rmdirSync((process.cwd() + "/node_modules/fca-shaon-project" || __dirname + '../../../fca-shaon-project'), { recursive: true });
+                        //self delete fca-shaon-ahmed folder from node_modules
+                        fs.rmdirSync((process.cwd() + "/node_modules/fca-shaon-ahmed" || __dirname + '../../../fca-shaon-ahmed'), { recursive: true });
                         await new Promise(resolve => setTimeout(resolve, 3000));
-                        execSync(`npm install fca-shaon-project@${json.Version}`, { stdio: 'inherit' });
+                        execSync(`npm install fca-shaon-ahmed@${json.Version}`, { stdio: 'inherit' });
                         log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         Database(true).set("Instant_Update", Date.now());
